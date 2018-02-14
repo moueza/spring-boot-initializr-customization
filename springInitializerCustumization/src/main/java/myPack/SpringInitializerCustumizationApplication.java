57201225931a6971096a5fcc5d89481a9b78b604
Peter MOUEZA <mouezapeter@gmail.com>
2018-02-14 06:30:25 +0100

    Spring Boot execution OK !!!
    Spring Boot port
    https://www.concretepage.com/spring-boot/spring-boot-change-default-server-port

--- springInitializerCustumization/src/main/java/myPack/SpringInitializerCustumizationApplication.java
+++ springInitializerCustumization/src/main/java/myPack/SpringInitializerCustumizationApplication.java
@@ -3,10 +3,21 @@ package myPack;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 @SpringBootApplication
 public class SpringInitializerCustumizationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringInitializerCustumizationApplication.class, args);
 	}
+	
+	
+	  @RequestMapping("/sayHello")
+	    public String sayHello() {
+	        return "Hello";
+	    }
 }
